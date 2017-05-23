d80bfa855ee2e00138ff97dd2c5e9fa3cbf340e6
Sanchit Agarwal <sanchit3008@gmail.com>
2017-05-23 16:35:38 +0530

    ohno

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 from apiai import ApiAI
 import json
 
-CLIENT_ACCESS_TOKEN = "727479e377c14bc293f88801c713a07f"
+CLIENT_ACCESS_TOKEN = "da359014f4414053b6bcf2192d05cbfa"
 
 ai = ApiAI(CLIENT_ACCESS_TOKEN)
 
 def response(message_text):
 	request = ai.text_request()
-	request.query = "who are you"
+	request.query = message_text
 	response = request.getresponse().read().decode('utf-8')
 	obj = json.loads(response)
 	return (obj["result"]["fulfillment"]["speech"])
