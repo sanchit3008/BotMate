b26f9a03ed3e07ad3f092168fa6f4d1cc56ba1f4
Sanchit Agarwal <sanchit3008@gmail.com>
2017-05-24 01:59:06 +0530

    added button

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def webhook():
 
 					if messaging_text[:3].lower() == "gif":
 						bot.send_image_url(sender_id, gif_search(messaging_text[4:]))
-					elif messaging_event['payload'] == "help":
+					elif messaging_event['postback']['payload'] == "help":
 						bot.send_text_message(sender_id, 'You can chat with me, and also search for gifs.Try typing "gif cats".')
 					else:
 						bot.send_text_message(sender_id, response(messaging_text))
