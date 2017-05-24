ff9a14f3aa19097044915bc3f185543d8321fe1c
Sanchit Agarwal <sanchit3008@gmail.com>
2017-05-25 04:02:08 +0530

    update utils.py

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ CLIENT_ACCESS_TOKEN = "YOUR_TOKEN"
 ai = ApiAI(CLIENT_ACCESS_TOKEN)
 
 def response(message_text):
+	#api.ai response
 	request = ai.text_request()
 	request.query = message_text
 	response = request.getresponse().read().decode('utf-8')
@@ -15,6 +16,7 @@ def response(message_text):
 
 
 def gif_search(message_text):
+	#giphy response
 	search_phrase = message_text.replace(" ", "-")
 	url = "http://api.giphy.com/v1/gifs/search?q=" + search_phrase + "&api_key=dc6zaTOxFJmzC&limit=1"
 	response = requests.get(url)
